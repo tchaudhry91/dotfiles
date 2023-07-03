ceefb23982439d7c5fbca03aac0810c1a7968b9b
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2023-07-03 11:26:48 +0530

    enable autoformat

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,11 +1,12 @@
 local lsp = require('lsp-zero').preset({})
 
 lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
+	lsp.default_keymaps({ buffer = bufnr })
 end)
 
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+lsp.buffer_autoformat()
 
 lsp.setup()
 
@@ -19,7 +20,7 @@ cmp.setup({
 		completeopt = 'menu,menuone,noinsert'
 	},
 	mapping = {
-		['<CR>'] = cmp.mapping.confirm({select = false}),
+		['<CR>'] = cmp.mapping.confirm({ select = false }),
 		['<C-Space>'] = cmp.mapping.complete(),
 	}
 })
