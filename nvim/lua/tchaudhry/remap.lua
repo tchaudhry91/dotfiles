6d2b18b03cdcbf807b5f562b460ebd3e22385e69
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2023-07-13 09:46:54 +0530

    Reconfigure HL

--- nvim/lua/tchaudhry/remap.lua
+++ nvim/lua/tchaudhry/remap.lua
@@ -5,6 +5,9 @@ vim.keymap.set("n", "<leader>f", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set("n", "H", "^")
+vim.keymap.set("n", "L", "$")
+
 vim.keymap.set("n", "J", "mzJ`z")
 
 -- Clipboard
