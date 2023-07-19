9bbe036a658b28c35800707f5bdfd5bcb39a80c3
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2023-07-19 13:09:06 +0530

    update format command

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -17,6 +17,28 @@ lsp.format_on_save({
 	}
 })
 
+lsp.on_attach(function(client, bufnr)
+	lsp.default_keymaps({ buffer = bufnr })
+	local opts = { buffer = bufnr }
+
+	vim.keymap.set({ 'n', 'x' }, 'gq', function()
+		vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+	end, opts)
+end)
+
+
+
+lsp.on_attach(function(client, bufnr)
+	lsp.default_keymaps({ buffer = bufnr })
+	local opts = { buffer = bufnr }
+
+	vim.keymap.set({ 'n', 'x' }, 'gq', function()
+		vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+	end, opts)
+end)
+
+lsp.setup()
+
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 require('lspconfig').rust_analyzer.setup {
 	settings = {
@@ -31,6 +53,15 @@ require('lspconfig').rust_analyzer.setup {
 		}
 	}
 }
+
+lsp.on_attach(function(client, bufnr)
+	lsp.default_keymaps({ buffer = bufnr })
+	local opts = { buffer = bufnr }
+
+	vim.keymap.set({ 'n', 'x' }, 'gq', function()
+		vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+	end, opts)
+end)
 
 lsp.setup()
 
