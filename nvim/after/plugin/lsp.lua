b18dcf3d0a73efe8f2f14c5dd5dc233e39bf168f
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2023-07-24 11:10:20 +0530

    update

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -33,15 +33,6 @@ require('lspconfig').rust_analyzer.setup {
 	}
 }
 
-lsp.on_attach(function(client, bufnr)
-	lsp.default_keymaps({ buffer = bufnr })
-	local opts = { buffer = bufnr }
-
-	vim.keymap.set({ 'n', 'x' }, 'gq', function()
-		vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
-	end, opts)
-end)
-
 lsp.setup()
 
 -- Make sure you setup `cmp` after lsp-zero
