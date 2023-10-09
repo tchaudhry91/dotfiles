01d60ab4e03e87d948df03dbc898283a7f14a6a1
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2023-10-09 22:42:56 +0530

    removed lsp-zero

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,25 +1,17 @@
-local lsp = require('lsp-zero').preset({})
+require("mason").setup()
+require("mason-lspconfig").setup(
+	{ ensure_installed = { "rust_analyzer", "lua_ls", "gopls", "svelte", "zls", "pyright" } })
 
-lsp.on_attach(function(client, bufnr)
-	lsp.default_keymaps({ buffer = bufnr })
-	vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { buffer = true })
-end)
 
-lsp.format_on_save({
-	format_opts = {
-		async = false,
-		timeout_ms = 10000,
-	},
-	servers = {
-		['rust_analyzer'] = { 'rust' },
-		['lua_ls'] = { 'lua' },
-		['black'] = { 'python' },
-		['gopls'] = { 'go' },
-		['svelte'] = { 'svelte' },
-	}
-})
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local lspconfig = require('lspconfig')
 
-require("lspconfig").rust_analyzer.setup {
+lspconfig.lua_ls.setup { capabilities = capabilities }
+
+lspconfig.gopls.setup { capabilities = capabilities }
+
+lspconfig.rust_analyzer.setup {
+	capabilities = capabilities,
 	settings = {
 		['rust-analyzer'] = {
 			checkOnSave = {
@@ -33,34 +25,54 @@ require("lspconfig").rust_analyzer.setup {
 	}
 }
 
-require('lspconfig').tsserver.setup({
-	on_attach = function(client, bufnr)
-		lsp.async_autoformat(client, bufnr)
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+	callback = function(ev)
+		-- Buffer local mappings.
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		local opts = { buffer = ev.buf }
+		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+		vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+		vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+		vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+		vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+		vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+		vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+		vim.keymap.set('n', '<leader>f', function()
+			vim.lsp.buf.format { async = true }
+		end, opts)
+		vim.keymap.set('n', '<leader>d', '<cmd>Telescope diagnostics<cr>', opts)
 	end,
 })
 
-require('lspconfig').pyright.setup({
-	on_attach = function(client, bufnr)
-		lsp.async_autoformat(client, bufnr)
-	end,
-})
 
-require('lspconfig').eslint.setup({})
+-- luasnip setup
+local luasnip = require 'luasnip'
 
-
-lsp.setup()
-
--- Make sure you setup `cmp` after lsp-zero
-
-local cmp = require('cmp')
-
-cmp.setup({
-	preselect = 'item',
-	completion = {
-		completeopt = 'menu,menuone,noinsert'
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
 	},
-	mapping = {
-		['<CR>'] = cmp.mapping.confirm({ select = false }),
+	mapping = cmp.mapping.preset.insert({
+		['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
+		['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
+		-- C-b (back) C-f (forward) for snippet placeholder navigation.
 		['<C-Space>'] = cmp.mapping.complete(),
-	}
-})
+		['<CR>'] = cmp.mapping.confirm {
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		},
+	}),
+	sources = {
+		{ name = 'nvim_lsp' },
+		{ name = 'luasnip' },
+	},
+}
