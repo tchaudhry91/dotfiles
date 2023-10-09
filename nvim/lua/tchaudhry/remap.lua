da4f60591d2a7e1944919be4a89142d74889aea4
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2023-10-09 22:47:07 +0530

    fix terminal

--- nvim/lua/tchaudhry/remap.lua
+++ nvim/lua/tchaudhry/remap.lua
@@ -2,7 +2,7 @@ vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set("t", "jk", "<C-\\><C-n>")
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>f", ":NvimTreeToggle<CR>")
+vim.keymap.set("n", "<leader>x", "<cmd>NvimTreeToggle<CR>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -17,4 +17,4 @@ vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+y")
 
-vim.keymap.set("n", "<leader>`", ":ToggleTerm<CR>")
+vim.keymap.set({ "n", "t", "x", "v" }, "<leader>`", "<cmd>ToggleTerm<CR>")
