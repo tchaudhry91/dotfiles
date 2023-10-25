1c383a42d9b6dffe3806968b551b9ffdaddf4466
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2023-10-25 13:15:50 +0530

    added tmuxifier

--- setup.sh
+++ setup.sh
@@ -9,16 +9,12 @@ rm -rf ~/.config/nvim || true
 
 cp -r ./nvim ~/.config/
 
-# Additional Step to install Packer
-# [ ! -d ~/.local/share/nvim/site/pack/packer/start/packer.nvim ] && git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
-
 # Add Tmux Conf
 rm -rf ~/.config/tmux/tmux.conf || true
 mkdir -p ~/.config/tmux || true
 cp -r ./tmux/tmux.conf ~/.config/tmux/tmux.conf
 
-[ ! -d ~/.tmux/plugins/tpm ] && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+[ ! -d ~/.config/tmux/plugins/tpm ] && git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 
 
 # Add Kitty conf
