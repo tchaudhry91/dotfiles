35fbadc3e6e0d5f0e2519b35503068d601ef2535
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2025-03-26 11:47:21 +0530

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,15 +8,19 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+config.front_end = "WebGpu"
+
 -- This is where you actually apply your config choices
 --
 config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Regular" })
-config.font_size = 16.0
+config.font_size = 11
 
 -- For example, changing the color scheme:
 config.color_scheme = "Tokyo Night Moon"
 
 config.hide_tab_bar_if_only_one_tab = true
+
+config.default_prog = { "Arch.exe" }
 
 -- and finally, return the configuration to wezterm
 return config
