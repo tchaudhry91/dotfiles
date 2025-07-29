0a62c19ba36e4358dfb7508c0ca00f67674f9cab
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2025-07-29 19:09:39 +0530

    test

--- nvim/init.lua
+++ nvim/init.lua
@@ -562,6 +562,7 @@ require('lazy').setup {
                 library = {
                   '${3rd}/luv/library',
                   unpack(vim.api.nvim_get_runtime_file('', true)),
+                  vim.env.VIMRUNTIME,
                 },
                 -- If lua_ls is really slow on your computer, you can try this instead:
                 -- library = { vim.env.VIMRUNTIME },
