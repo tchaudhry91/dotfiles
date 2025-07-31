8f4228d89ae0f4df136ec9d9f949be9020e615fe
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2025-07-31 11:21:02 +0530

    update claude agents setup

--- setup.sh
+++ setup.sh
@@ -12,6 +12,8 @@ cp -r ./nvim ~/.config/
 [ ! -d ~/.config/tmux/plugins/tpm ] && git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 cp ./tmux/tmux.conf ~/.tmux.conf
 
+mkdir -p ~/.cladue/agents || true
+cp claude-agents/* ~/.claude/agents/
 
 
 # Add Kitty conf
