a9862f6537dc264af9f39c25fb1549b2b89ff23a
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2026-09-19 14:18:58 +0530

    nvim: drop dead kickstart.nvim boilerplate
    Remove unused upstream components that were never loaded:
      README.md, LICENSE.md, doc/kickstart.txt  (kickstart docs/license)
      lua/kickstart/health.lua                 (health check module)
      lua/kickstart/plugins/debug.lua          (nvim-dap, commented out)
      lua/kickstart/plugins/indent_line.lua    (commented out)
    
    Also strip the commented-out kickstart.plugins requires from init.lua.
    Custom plugins/keymaps (nvim-tree, toggleterm, trouble, tmux-nav,
    zen-mode, Arborist) are untouched.

--- nvim/init.lua
+++ nvim/init.lua
@@ -820,19 +820,7 @@ require('lazy').setup {
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- put them in the right spots if you want.
 
-  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for kickstart
-  --
-  --  Here are some example plugins that I've included in the kickstart repository.
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
-  --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
-  --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
+  -- Load your own plugins/configuration from `lua/custom/plugins/*.lua`
   { import = 'custom.plugins' },
 }
 
